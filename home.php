d48af2a24554d3f267cabd9a478e067e60d78636
Mark Fominykh <mark545154@mail.ru>
2021-12-29 15:22:37 +0700

    Initial commit

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Home page
+*/
+?>
+
+<?php get_header(); ?>
+
+    <div class="container">
+        <h1 class="title"> Привет, мир!</h1>
+        <p class="text">Это стартовый шаблон, который можете изменить или удалить.</p>
+
+    </div>
+
+
+
+<?php get_footer(); ?>
